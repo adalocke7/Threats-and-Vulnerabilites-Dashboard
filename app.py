a84005c57f11ec08225d7ee13733ae43ebab0a0b
yuzuruus <72951653+yuzuruus@users.noreply.github.com>
2026-08-17 15:34:07 -0700

    Add template for lab html and fix some styling

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html', catalog=catalog)
 
+@app.route('/lab')
+def lab():
+    return render_template('lab.html', catalog=catalog)
+
 if __name__ == '__main__':
     app.run(debug=True)
